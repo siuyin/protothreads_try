d30c531bcc925737645f69bee69a69dfc53e3250
Loh Siu Yin <siuyin@beyondbroadcast.com>
2025-01-25 01:16:16 +0800

    feat: add print to console protothread

--- hello_protothreads/hello_protothreads.c
+++ hello_protothreads/hello_protothreads.c
@@ -5,29 +5,43 @@
 // use defines to make more meaningful names for our GPIO pins
 #define PIN_1 PD0
 
-volatile uint32_t start_tick;
-static uint32_t on_ticks = Ticks_from_Ms(250);
-static uint32_t off_ticks = Ticks_from_Ms(250);
+volatile uint32_t bl_start_tick;
+static uint32_t bl_on_ticks = Ticks_from_Ms(250);
+static uint32_t bl_off_ticks = Ticks_from_Ms(500);
 
-static int protothread1(struct pt *pt) {
+static int blink_led(struct pt *pt) {
 	PT_BEGIN(pt);
 	while(1) {
-		start_tick = SysTick->CNT;
+		bl_start_tick = SysTick->CNT;
 		funDigitalWrite(PIN_1, FUN_HIGH);
-		PT_WAIT_UNTIL(pt, SysTick->CNT - start_tick >= on_ticks);
+		PT_WAIT_UNTIL(pt, SysTick->CNT - bl_start_tick >= bl_on_ticks);
 		funDigitalWrite(PIN_1, FUN_LOW);
-		start_tick = SysTick->CNT;
-		PT_WAIT_UNTIL(pt, SysTick->CNT - start_tick >= off_ticks);
+		bl_start_tick = SysTick->CNT;
+		PT_WAIT_UNTIL(pt, SysTick->CNT - bl_start_tick >= bl_off_ticks);
 	}
 	PT_END(pt);
 }
 
-static struct pt pt1;
+volatile uint32_t pr_start_tick;
+static uint32_t pr_intvl_ticks = Ticks_from_Ms(1000);
+
+static int prnt(struct pt *pt) {
+	PT_BEGIN(pt);
+	while(1) {
+		pr_start_tick = SysTick->CNT;
+		printf(".");
+		PT_WAIT_UNTIL(pt, SysTick->CNT - pr_start_tick >= pr_intvl_ticks);
+	}
+	PT_END(pt);
+}
+
+static struct pt pt_bl, pt_pr;
 
 int main()
 {
 	SystemInit();
-	PT_INIT(&pt1);
+	PT_INIT(&pt_bl);
+	PT_INIT(&pt_pr);
 
 	// Enable GPIOs
 	funGpioInitAll();
@@ -36,7 +50,8 @@ int main()
 
 	while(1)
 	{
-		protothread1(&pt1);
+		blink_led(&pt_bl);
+		prnt(&pt_pr);
 		// funDigitalWrite( PIN_1,     FUN_HIGH );
 		// Delay_Ms( 250 );
 		// funDigitalWrite( PIN_1,     FUN_LOW );
