da02a087c3bafee05293499645ccfa064356abed
Loh Siu Yin <siuyin@beyondbroadcast.com>
2025-01-25 22:16:10 +0800

    feat: synchronize threads

--- hello_protothreads/hello_protothreads.c
+++ hello_protothreads/hello_protothreads.c
@@ -3,41 +3,38 @@
 #include <stdio.h>
 
 // use defines to make more meaningful names for our GPIO pins
-#define PIN_1 PD0
+#define LED PD0
 
+#include "proto_delay.h"
 
-#define INTVL_MS 125
-#define DELAY(dly) {\
-	bl_start_tick = SysTick->CNT;\
-	PT_WAIT_UNTIL(pt, SysTick->CNT - bl_start_tick >= Ticks_from_Ms(dly));\
-}
-
+#define INTVL_MS 500 
+volatile static uint32_t bl_start_tick;
+static uint8_t pr_start;
 static int blink_led(struct pt *pt) {
-	volatile static uint32_t bl_start_tick;
-	//static uint32_t bl_ticks = Ticks_from_Ms(INTVL_MS);
 	PT_BEGIN(pt);
 	while(1) {
-		// bl_start_tick = SysTick->CNT;
-		funDigitalWrite(PIN_1, FUN_HIGH);
-		// PT_WAIT_UNTIL(pt, SysTick->CNT - bl_start_tick >= bl_ticks);
-		DELAY(INTVL_MS);
-		funDigitalWrite(PIN_1, FUN_LOW);
-		//bl_start_tick = SysTick->CNT;
-		//PT_WAIT_UNTIL(pt, SysTick->CNT - bl_start_tick >= bl_ticks);
-		DELAY(INTVL_MS);
+		PT_WAIT_UNTIL(pt,pr_start == 0);
+
+		funDigitalWrite(LED, FUN_HIGH);
+		DELAY(bl_start_tick, INTVL_MS);
+		funDigitalWrite(LED, FUN_LOW);
+		DELAY(bl_start_tick, INTVL_MS);
+
+		pr_start=1;
 	}
 	PT_END(pt);
 }
 
 volatile uint32_t pr_start_tick;
-static uint32_t pr_intvl_ticks = Ticks_from_Ms(1000);
-
 static int prnt(struct pt *pt) {
+static uint8_t n = 0;
 	PT_BEGIN(pt);
 	while(1) {
-		pr_start_tick = SysTick->CNT;
-		printf(".");
-		PT_WAIT_UNTIL(pt, SysTick->CNT - pr_start_tick >= pr_intvl_ticks);
+		PT_WAIT_UNTIL(pt, pr_start == 1); 
+
+		printf("%d\n",n++);
+
+		pr_start = 0;
 	}
 	PT_END(pt);
 }
@@ -53,16 +50,11 @@ int main()
 	// Enable GPIOs
 	funGpioInitAll();
 	
-	funPinMode( PIN_1,     GPIO_Speed_10MHz | GPIO_CNF_OUT_PP );
+	funPinMode( LED,     GPIO_Speed_10MHz | GPIO_CNF_OUT_PP );
 
-#define DELAY_TIME 250
 	while(1)
 	{
 		blink_led(&pt_bl);
 		prnt(&pt_pr);
-		// funDigitalWrite( PIN_1,     FUN_HIGH );
-		// Delay_Ms( 250 );
-		// funDigitalWrite( PIN_1,     FUN_LOW );
-		// Delay_Ms( 250 );
 	}
 }
